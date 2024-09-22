3cf6bd8117060a38ac1ee46f517aa1f2e2474d3e
Stack mastery <labib.developers@gmail.com>
2024-09-23 00:24:31 +0600

    header done with tabs

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Element Selection Into Header
+const headerBtn = document.getElementById('headerBtn'); // Header Button
+
+// Header Button href System
+headerBtn.addEventListener("click", function() {
+    window.location.href = './blog';
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary : "#B4F461",
+        offWhite: "rgba(17, 17, 17, 0.05)",
+        textPrimary: "#111111",
+        textSecondary: "rgba(17, 17, 17, 0.7)",
+        borderColor: "rgba(17, 17, 17, 0.1) ",
+      },
+      width:{
+        primary: "1440px"
+      },
+      fontFamily:{
+        primary: "Lexend"
+      },
+    },
   },
   plugins: [],
 }
